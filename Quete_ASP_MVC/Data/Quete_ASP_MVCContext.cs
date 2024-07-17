6fca332f8d1760a5fa29ad0f4b23fa106951204e
ABT Aurelien <aurelien@abrassart.net>
2024-07-17 09:40:47 +0200

    Ajoutez des fichiers projet.

---
+++ Quete_ASP_MVC/Data/Quete_ASP_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Quete_ASP_MVC.Models;
+
+namespace Quete_ASP_MVC.Data
+{
+    public class Quete_ASP_MVCContext : DbContext
+    {
+        public Quete_ASP_MVCContext (DbContextOptions<Quete_ASP_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Quete_ASP_MVC.Models.Article> Article { get; set; } = default!;
+    }
+}
---
+++ Quete_ASP_MVC/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Quete_ASP_MVC.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
